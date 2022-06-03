1157256e302778c7423f42d8b4c437bee955e014
Abhishek Kumar <abhishekpawan297@gmail.com>
2022-06-03 17:26:44 +0530

    preparing for deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express");
 const cors = require("cors")
 const jsonServer = require("json-server");
@@ -8,15 +9,19 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
-
 const app = express();
 const db = generatingDB();
 const {protect} = require('./middleware/authMiddleware')
-app.use(cors())
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api",protect, jsonServer.defaults(), jsonServer.router(db));
+
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
